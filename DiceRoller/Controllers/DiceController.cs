cbead928bac6733dbd7efa275c4cc4df96260c00
Jens Christian Rasch <jcr@wodecraft.dk>
2016-09-08 10:59:31 +0200

    Commit of point 1-4

---
+++ DiceRoller/Controllers/DiceController.cs
@@ -0,0 +1,39 @@
+﻿using DiceRoller.Models;
+using System.Web.Mvc;
+
+namespace DiceRoller.Controllers
+{
+    public class DiceController : Controller
+    {
+        // GET: Dice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            Dice dice;
+            if (Session["dice"] == null)
+            {
+                dice = new Dice();
+                dice.Roll();
+                Session["dice"] = dice;
+            }
+            else
+            {
+                dice = (Dice)Session["dice"];
+                dice.Roll();
+            }
+
+            ViewBag.LatestRoll = dice.Outcome;
+            ViewBag.NumRolls = dice.NumRolls;
+            ViewBag.TotalSum = dice.TotalSum;
+            ViewBag.AvgScore = (dice.TotalSum / dice.NumRolls);
+            ViewBag.Distribution = dice.Distribution;
+
+            return View();
+        }
+    }
+}
---
+++ DiceRoller/Models/Dice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DiceRoller.Models
+{
+    public class Dice
+    {
+
+        private int eyes;
+        private int numRolls;
+        private int outcome;
+        private int totalSum;
+        private int[] distribution;
+
+        public int NumRolls
+        {
+            get
+            {
+                return numRolls;
+            }
+        }
+
+        public int Outcome
+        {
+            get
+            {
+                return outcome;
+            }
+        }
+
+        public int TotalSum
+        {
+            get
+            {
+                return totalSum;
+            }
+        }
+
+        public int[] Distribution
+        {
+            get
+            {
+                return distribution;
+            }
+        }
+
+
+        Random rnd = new Random();
+
+        public Dice()
+        {
+            eyes = 6;
+            numRolls = 0;
+            totalSum = 0;
+            distribution = new int[eyes];
+        }
+
+        public void Roll()
+        {
+            numRolls++;
+            outcome = rnd.Next(1, 7);
+            totalSum += outcome;
+            distribution[outcome - 1]++;
+        }
+
+    }
+}
